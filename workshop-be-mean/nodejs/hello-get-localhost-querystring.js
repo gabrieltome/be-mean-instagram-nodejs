14e058a61d0abf76730bd7a4d085822a03e8914c
gabrieltome <gabrieltsistemas@hotmail.com>
2016-05-09 12:42:17 -0300

    add example files

---
+++ workshop-be-mean/nodejs/hello-get-localhost-querystring.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const http = require('http');
+
+http.get({
+    hostname: 'localhost',
+    path: '/user?name=Gabriel&aluno=true&age=27',
+    port: 3000,
+    agent: false
+}, (response) => {
+  let body = '';
+  console.log('STATUS:' + response.statusCode);
+  console.log('HEADERS:' + response.headers);
+
+  response.on('data', function(data){
+    body += data;
+  });
+  
+  response.on('end', function(){
+    console.log('Resposta:', body);
+  });
+
+});
---
+++ workshop-be-mean/nodejs/http-request.js
@@ -0,0 +1,40 @@
+// create REQUEST
+
+'use strict';
+
+const http = require('http');
+
+// JSON options de configuração
+const options = {
+  host: 'api.redtube.com',
+  path: '/?data=redtube.Videos.searchVideos&search=Sasha%20Gray'
+};
+
+// função de callback
+function callback(res) {
+  console.log('STATUS:' + res.statusCode);
+  console.log('HEADERS:' + JSON.stringify(res.headers));
+
+  let data = '';
+
+  res.setEncoding('utf8');
+
+  res.on('data', (chunk) => {
+    data += chunk;
+  });
+
+  res.on('end', () => {
+    console.log('Dados finalizados:', data);
+  });
+}
+
+// criando a request utilizando options e o callback
+const req = http.request(options, callback);
+
+// cria o error
+req.on('error', (e) => {
+  console.log('ERRO:' + e.message);
+});
+
+//finalizando...
+req.end();
